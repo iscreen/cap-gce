c0f3d6ce9f4d72962de0f994c432a1a5d3bdd0b8
ammeil <amme@ing.dk>
2019-02-22 14:16:28 +0200

    check if there are some vm in specified zone

--- lib/cap-gce/gce-handler.rb
+++ lib/cap-gce/gce-handler.rb
@@ -69,10 +69,12 @@ module CapGCE
     def get_servers_for_role(role)
       servers = []
       @gce.each do |_, gce|
-        servers += gce.items.select do |i|
-          instance_has_tag?(i, roles_tag, role) &&
-            instance_has_tag?(i, stages_tag, stage) &&
-            instance_has_tag?(i, project_tag, application)
+        unless gce.items != nil
+          servers += gce.items.select do |i|
+            instance_has_tag?(i, roles_tag, role) &&
+              instance_has_tag?(i, stages_tag, stage) &&
+              instance_has_tag?(i, project_tag, application)
+          end
         end
       end
       servers.sort_by(&:name)
