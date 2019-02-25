f38e524ffb6f932926eeb1896bb18a5ef31ecaf1
ammeil <amme@ing.dk>
2019-02-25 11:00:28 +0200

    https://github.com/iscreen/cap-gce/issues/5 use `next` in loop instead of 'unless'. ref https://github.com/iscreen/cap-gce/pull/6#discussion_r259695010

--- lib/cap-gce/gce-handler.rb
+++ lib/cap-gce/gce-handler.rb
@@ -69,12 +69,11 @@ module CapGCE
     def get_servers_for_role(role)
       servers = []
       @gce.each do |_, gce|
-        unless gce.items == nil
-          servers += gce.items.select do |i|
-            instance_has_tag?(i, roles_tag, role) &&
-              instance_has_tag?(i, stages_tag, stage) &&
-              instance_has_tag?(i, project_tag, application)
-          end
+        next if gce.items.nil?
+        servers += gce.items.select do |i|
+          instance_has_tag?(i, roles_tag, role) &&
+            instance_has_tag?(i, stages_tag, stage) &&
+            instance_has_tag?(i, project_tag, application)
         end
       end
       servers.sort_by(&:name)
